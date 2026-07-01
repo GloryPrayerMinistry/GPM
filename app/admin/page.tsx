acad47b5b0831e69b0c9815baf75312558f4f548
Sanehaa <133956348+Sanehaa@users.noreply.github.com>
2026-07-02 00:20:06 +0100

    not new commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Link from 'next/link';
 import { prisma } from '@/app/lib/db';
 import { BookOpen, ShoppingBag, MessageSquareQuote, Clock, ImageIcon, CalendarRange } from 'lucide-react';
